6cb8335b9e3c0e25575df8f4c0ace95c4a13f3b2
anshul kumar <anshullakra8@gmail.com>
2026-07-23 14:54:13 +0530

    Time: 111 ms (17.49%), Space: 117 MB (10.4%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,18 +1,23 @@
+#include <unordered_set>
+#include <vector>
+
 class Solution {
 public:
     bool containsNearbyDuplicate(std::vector<int>& nums, int k) {
-        std::unordered_map<int, int> seen;
+        std::unordered_set<int> window;
         
         for (int i = 0; i < nums.size(); i++) {
-            // Check if the current number was seen before
-            if (seen.find(nums[i]) != seen.end()) {
-                // Check if the distance between indices is <= k
-                if (i - seen[nums[i]] <= k) {
-                    return true;
-                }
+            // Check if current element is in our sliding window
+            if (window.count(nums[i])) {
+                return true;
             }
-            // Update the last seen index of nums[i]
-            seen[nums[i]] = i;
+            
+            window.insert(nums[i]);
+            
+            // Keep window size at most k
+            if (window.size() > k) {
+                window.erase(nums[i - k]);
+            }
         }
         
         return false;
