44b236a980b244808cecb4ca908241f7d30a6473
anshul kumar <anshullakra8@gmail.com>
2026-08-01 09:55:54 +0530

    Time: 95 ms (19.36%), Space: 9.4 MB (96.48%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,29 +1,23 @@
 
 
 class Solution {
+private:
+    // Helper function returning max relative score difference player can get from nums[i...j]
+    int maxDiff(const std::vector<int>& nums, int i, int j) {
+        // Base case: only one element left
+        if (i == j) {
+            return nums[i];
+        }
+
+        // Pick left element OR right element
+        int takeLeft = nums[i] - maxDiff(nums, i + 1, j);
+        int takeRight = nums[j] - maxDiff(nums, i, j - 1);
+
+        return std::max(takeLeft, takeRight);
+    }
+
 public:
     bool predictTheWinner(std::vector<int>& nums) {
-        int n = nums.size();
-        
-        // dp[j] will represent the maximum score difference Player 1 can achieve 
-        // over Player 2 for the subarray nums[i...j].
-        std::vector<int> dp(n, 0);
-
-        // Base case: Subarrays of length 1 (i == j)
-        for (int i = 0; i < n; ++i) {
-            dp[i] = nums[i];
-        }
-
-        // Fill DP table for larger subarray lengths from right to left
-        for (int i = n - 2; i >= 0; --i) {
-            for (int j = i + 1; j < n; ++j) {
-                // Taking nums[i] leaves range [i+1, j]
-                // Taking nums[j] leaves range [i, j-1]
-                dp[j] = std::max(nums[i] - dp[j], nums[j] - dp[j - 1]);
-            }
-        }
-
-        // Player 1 wins if the max score difference for nums[0...n-1] is >= 0
-        return dp[n - 1] >= 0;
+        return maxDiff(nums, 0, nums.size() - 1) >= 0;
     }
 };
