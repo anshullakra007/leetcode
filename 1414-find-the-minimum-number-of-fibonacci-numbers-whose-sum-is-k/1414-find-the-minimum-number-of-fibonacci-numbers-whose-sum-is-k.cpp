d5bbcf75bd374ac3a7ce6161ed3f4bf2dcd8ad40
anshul kumar <anshullakra8@gmail.com>
2026-08-07 17:32:42 +0530

    Time: 0 ms (100%), Space: 8.9 MB (49.23%) - LeetHub

--- 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.cpp
+++ 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int findMinFibonacciNumbers(int k) {
+        // Step 1: Saare Fibonacci numbers k tak generate karke array me daal lo
         vector<int> fib = {1, 1};
-
-        // Step 1: Generate Fibonacci numbers up to k
         while (fib.back() < k) {
             fib.push_back(fib[fib.size() - 1] + fib[fib.size() - 2]);
         }
-
+        
         int count = 0;
-
-        // Step 2: Greedily subtract the largest possible Fibonacci numbers
+        
+        // Step 2: Peeche (sabse bade) se check karo aur k me se minus karte jao
         for (int i = fib.size() - 1; i >= 0; i--) {
             if (fib[i] <= k) {
                 k -= fib[i];
                 count++;
             }
         }
-
+        
         return count;
     }
 };
