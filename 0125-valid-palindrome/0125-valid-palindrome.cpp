65275a3404624d0a0f6fb896460628399c2eef3c
anshul kumar <anshullakra8@gmail.com>
2026-09-06 17:03:06 +0530

    Time: 1 ms (43.12%), Space: 9.9 MB (52.65%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0, r = s.size() - 1;
+
+        while (l < r) {
+            // Skip non-alphanumeric characters from the left
+            while (l < r && !isalnum(s[l])) {
+                l++;
+            }
+            // Skip non-alphanumeric characters from the right
+            while (l < r && !isalnum(s[r])) {
+                r--;
+            }
+
+            // Compare case-insensitively
+            if (tolower(s[l]) != tolower(s[r])) {
+                return false;
+            }
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+};
