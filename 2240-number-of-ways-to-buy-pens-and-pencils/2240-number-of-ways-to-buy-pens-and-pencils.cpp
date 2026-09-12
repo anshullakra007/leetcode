5f08d471dd2a33d0695595660e9e64d6a0f5b56a
anshul kumar <anshullakra8@gmail.com>
2026-09-12 14:53:40 +0530

    Time: 11 ms (31.87%), Space: 8 MB (17.76%) - LeetHub

--- 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.cpp
+++ 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.cpp
@@ -3,7 +3,7 @@ public:
     long long waysToBuyPensPencils(int total, int cost1, int cost2) {
         long long ans=0;
         for( int i=0;i*cost1<=total;i++){
-            int rem=total-i*cost1;
+            long long  rem=total-i*cost1;
             ans+=(rem/cost2)+1;
 
         }
