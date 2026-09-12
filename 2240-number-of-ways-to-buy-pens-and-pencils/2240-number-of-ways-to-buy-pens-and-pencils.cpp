fa98c92a78abfa9427d1809100d47ead75c20837
anshul kumar <anshullakra8@gmail.com>
2026-09-12 14:54:41 +0530

    Time: 6 ms (90.02%), Space: 7.9 MB (52.31%) - LeetHub

--- 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.cpp
+++ 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.cpp
@@ -1,13 +1,21 @@
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
 class Solution {
 public:
     long long waysToBuyPensPencils(int total, int cost1, int cost2) {
-        long long ans=0;
-        for( int i=0;i*cost1<=total;i++){
-            long long  rem=total-i*cost1;
-            ans+=(rem/cost2)+1;
-
-        }
-        return ans;
+        // Always iterate over the item with the higher price to minimize loop passes
+        int max_cost = max(cost1, cost2);
+        int min_cost = min(cost1, cost2);
         
+        long long ways = 0;
+        for (int rem = total; rem >= 0; rem -= max_cost) {
+            ways += (rem / min_cost) + 1;
+        }
+        
+        return ways;
     }
 };
